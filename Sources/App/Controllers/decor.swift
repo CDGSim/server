7ccf3fe3a9af638781e82af2bf2e6b5095b63e4a
Axel Péju <axel.peju@aviation-civile.gouv.fr>
2021-02-16 15:25:20 +0100

    DECOR improvements

--- Sources/App/Controllers/decor.swift
+++ Sources/App/Controllers/decor.swift
@@ -30,6 +30,8 @@ struct DecorController {
         var southRunway1RVR: Int = 9999
         var southRunway2RVR: Int = 9999
         var ceiling: Int = 10000
+        var cloudLayers: [String] = []
+        var weatherEvents: [String] = []
         
         init(from metarString:String) {
             let components = metarString.components(separatedBy: " ")
@@ -57,9 +59,10 @@ struct DecorController {
                 // Decode visibility
                 if component.count == 4 || component == "CAVOK" {
                     if component == "CAVOK" {
-                        visibility = 9999
-                    } else {
-                        visibility = Int(component) ?? 9999
+                        visibility = 10000
+                        cloudLayers = ["CAVOK"]
+                    } else if let value = Int(component) {
+                        visibility = value
                     }
                 }
                 
@@ -95,11 +98,55 @@ struct DecorController {
                 }
                 
                 // Decode ceiling
-                if component.count == 6 && ( component.prefix(3) == "OVC" || component.prefix(3) == "BKN" ) {
-                    let value = component.suffix(from: component.firstIndex(where: { "0"..."9" ~= $0 })!)
-                    ceiling = (Int(value) ?? 0) * 100
+                if component.count == 6 && ["OVC", "BKN", "FEW", "SCT"].contains(component.prefix(3)) {
+                    cloudLayers.append(component)
+                    if ["OVC", "BKN"].contains(component.prefix(3)){
+                        let value = component.suffix(from: component.firstIndex(where: { "0"..."9" ~= $0 })!)
+                        ceiling = min(ceiling, (Int(value) ?? 0) * 100)
+                    }
+                }
+                
+                // Weather events
+                let weatherEventComponents = ["VC", "MI", "PR", "DR", "BL", "FZ", "RE", "BC", "SH", "XX",
+                                              "RA", "SN", "GR", "DZ", "PL", "GS", "SG", "IC", "UP",
+                                              "BR", "FG", "HZ", "FU", "SA", "DU", "VA",
+                                              "PO", "SS", "DS", "SQ", "FC", "TS",
+                                              "+", "-", ""]
+                let possibleWeatherEvents = weatherEventComponents.map { firstComponent -> [String] in
+                    return weatherEventComponents.map { secondComponent -> String in
+                        firstComponent+secondComponent
+                    }
+                }.flatMap { $0 }
+                if possibleWeatherEvents.contains(component) {
+                    weatherEvents.append(component)
                 }
             }
+        }
+        
+        var readable: String {
+            let visibilityString: String
+            if visibility >= 9999 && cloudLayers.contains("CAVOK") {
+                visibilityString = ""
+            } else if visibility >= 9999 {
+                visibilityString = "10km"
+            } else if visibility >= 1000 {
+                visibilityString = "\(visibility/1000)km"
+            } else if visibility > 0 {
+                visibilityString = "\(visibility)m"
+            } else {
+                visibilityString = ""
+            }
+            let cloudStrings = cloudLayers.compactMap { layer -> String? in
+                guard let valueIndex = layer.firstIndex(where: { "0"..."9" ~= $0 }) else {
+                    return layer
+                }
+                let value = layer.suffix(from: valueIndex)
+                guard let height = Int(value) else {
+                    return nil
+                }
+                return "\(layer.prefix(3)) \(height*100)ft"
+            }.joined(separator: " ")
+            return "\(visibilityString) \(weatherEvents.joined(separator: " ")) \(cloudStrings)"
         }
     }
     
@@ -117,17 +164,30 @@ struct DecorController {
         let windDirection = weather.windDirection
         let windSpeed = weather.windSpeed
         
-        let northWindDirection = windDirection + Int.pseudoRandom(in: -1...3, changeEvery: 5)*5
-        let southWindDirection = windDirection - Int.pseudoRandom(in: -2...1, changeEvery: 6)*5
+        var northWindDirection = windDirection + Int.pseudoRandom(in: -1...2, changeEvery: 5)*10
+        while northWindDirection <= 0 {
+            northWindDirection += 360
+        }
+        while northWindDirection > 360 {
+            northWindDirection -= 360
+        }
+        var southWindDirection = windDirection - Int.pseudoRandom(in: -2...1, changeEvery: 4)*10
+        while southWindDirection <= 0 {
+            southWindDirection += 360
+        }
+        while southWindDirection > 360 {
+            southWindDirection -= 360
+        }
         let northWindSpeed = max(windSpeed + Int.pseudoRandom(in: -1...8, changeEvery: 2), 0)
         let southWindSpeed = max(windSpeed + Int.pseudoRandom(in: -2...7, changeEvery: 3), 0)
         
-        let northWind = DecorContext.Wind(direction: northWindDirection, speed: northWindSpeed)
-        let southWind = DecorContext.Wind(direction: southWindDirection, speed: southWindSpeed)
+        let northWind = DecorContext.Wind(direction: northWindDirection, speed: northWindSpeed, gust: weather.windGust)
+        let southWind = DecorContext.Wind(direction: southWindDirection, speed: southWindSpeed, gust: weather.windGust)
         
         // ATIS Letter
         let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         let atispg = String(letters[letters.index(letters.startIndex, offsetBy: Int.pseudoRandom(in: 0...25, changeEvery: 40) % 26)])
+        let atisol = String(letters[letters.index(letters.startIndex, offsetBy: 7 + Int.pseudoRandom(in: 0...20, changeEvery: 50) % 26)])
         let atislb = String(letters[letters.index(letters.startIndex, offsetBy: 5 + Int.pseudoRandom(in: 0...20, changeEvery: 50) % 26)])
         
         // Transition level
@@ -155,6 +215,7 @@ struct DecorController {
             struct Wind: Encodable {
                 let direction:Int
                 let speed:Int
+                let gust:Int?
             }
             
             struct RunwayRVR: Encodable {
@@ -164,13 +225,13 @@ struct DecorController {
             }
             
             let qnh:String
-            let qfe:String
             let transitionLevel:Int
             let temperature:Int
             let dewPoint:Int
             let northWind:Wind
             let southWind:Wind
             let atispg:String
+            let atisol:String
             let atislb:String
             let configuration:String
             let weather:String
@@ -193,16 +254,16 @@ struct DecorController {
         pressureFormatter.minimumIntegerDigits = 4
         
         return req.view.render("decor", DecorContext(qnh: pressureFormatter.string(from: NSNumber(value: qnh)) ?? "",
-                                                     qfe: pressureFormatter.string(from: NSNumber(value: qnh - 14)) ?? "",
                                                      transitionLevel: transitionLevel,
                                                      temperature: weather.temperature,
                                                      dewPoint: weather.dewPoint,
                                                      northWind: northWind,
                                                      southWind: southWind,
                                                      atispg: atispg,
+                                                     atisol: atisol,
                                                      atislb: atislb,
                                                      configuration: configuration,
-                                                     weather:metar,
+                                                     weather:weather.readable,
                                                      startTime:startTime,
                                                      northRunway1RVR:.init(start: northRunway1RVR + (1 + Int.pseudoRandom(in: -2...1, changeEvery: 2))*Int.pseudoRandom(in: 0...2, changeEvery: 2)*25,
                                                                            mid: northRunway1RVR + Int.pseudoRandom(in: -1...2, changeEvery: 2)*Int.pseudoRandom(in: 0...2, changeEvery: 2)*25,
