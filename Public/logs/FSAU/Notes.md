6555cc5629fb4f97f853524ae6d6d87899b57cd8
Axel Péju <axel.peju@aviation-civile.gouv.fr>
2021-02-24 17:07:17 +0100

    Implement course notes, navigation on the front end is now divided between pages for the instructors and pages for the pilots

---
+++ Public/logs/FSAU/Notes.md
@@ -0,0 +1,25 @@
+Partition à sélectionner pour l’ensemble des exercices : **FSAU_1**
+
+Des **captures d’écran** DECOR sont disponibles pour toutes les simulations. Penser à les aimanter au bon moment en face de chaque contrôleur.
+
+Pour chaque simulation, une distribution PC est proposée dans un tableau en haut de page. 
+
+### Pour afficher les protections d’attente si besoin
+
+(voir aussi feuille explicative plastifiée en salle ELECTRA) 
+
+dans le bureau CAUTRA (en bas à gauche de l’écran)\
+dans la fenêtre VISAS.VICDG\
+entrer le texte : \
+`XZONE AHNE ACT`  (affiche lorni)\
+`XZONE ANO ACT`  (affiche mopar)\
+`XZONE AHSE ACT`  (affiche okipa)\
+`XZONE AHSO ACT` (affiche banox)
+
+Plusieurs attentes peuvent être activées en même temps : `XZONE AHNE ANO … ACT`
+
+### Pour retirer les protections d’attente
+dans le bureau CAUTRA (en bas à gauche de l’écran)\
+dans la fenêtre VISAS.VICDG\
+entrer le texte : \
+`XZONE AHNE ANN`  (retire lorni) etc.
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -141,9 +141,111 @@ func registerFrontEndRoutes(_ app: Application) throws {
         return req.view.render("index", context)
     }
     
-    // MARK: GET /instructor/log_path
+    // MARK: GET /instructeur
+    // Renders a view containing all the courses listed in alphabetical order
+    app.get("instructeur") { req -> EventLoopFuture<View> in
+        
+        // Context type that will be passed to the view
+        struct Context: Encodable {
+            let courses: [Course]
+            
+            struct Course: Encodable {
+                let name: String
+            }
+        }
+        
+        // Enumerate files in the logs folder
+        let enumerator = FileManager.default.enumerator(atPath: "Public/logs")
+        guard let subpaths = enumerator?.allObjects as? [String]  else {
+            // If we cannot get the subpaths, render the error view
+            return req.view.render("error")
+        }
+        
+        let courses = subpaths.compactMap { path -> String? in
+            // Only include the subfolders
+            guard path.components(separatedBy: "/").count == 1 else { return nil }
+         
+            // Exclude hidden folders
+            guard path.first != "." else { return nil }
+            
+            return path
+        }
+        .sorted(by: <)
+        .map { path -> Context.Course in
+            .init(name: path)
+        }
+        
+        // Render the view index, with the context we've made
+        let context:Context = .init(courses: courses)
+        return req.view.render("instructor/index", context)
+    }
+    
+    // MARK: GET /stage/NOM_DU_STAGE
+    // Renders a view containing all the simulations of the specified course
+    app.get("stage", ":courseName") { req -> EventLoopFuture<View> in
+        guard let courseName = req.parameters.get("courseName") else {
+            return req.view.render("error")
+        }
+        
+        // Context type that will be passed to the view
+        struct Context: Encodable {
+            let name: String
+            let simulations: [Simulation]
+            let notes: String
+            
+            struct Simulation: Encodable {
+                let name: String
+                let group: String?
+                let path: String
+            }
+        }
+        
+        // Enumerate files in the logs folder
+        let enumerator = FileManager.default.enumerator(atPath: "Public/logs/" + courseName)
+        guard let subpaths = enumerator?.allObjects as? [String]  else {
+            // If we cannot get the subpaths, render the error view
+            return req.view.render("error")
+        }
+        
+        
+        let simulations = subpaths.compactMap { path -> String? in
+            // Filter files to only include .simlog files
+            guard URL(fileURLWithPath: path).pathExtension == "simlog" else { return nil }
+            
+            return path
+        }.compactMap { path -> Context.Simulation? in
+            // Read the simulation name from the log file
+            switch log(atPath: courseName + "/" + path) {
+            case .failure : return nil // If the log file cannot be read, just ignore it
+            case .success(let log):
+                let pathComponents = path.components(separatedBy: "/")
+                let name = log.properties.name
+                let group:String?
+                if pathComponents.count > 1 { group = pathComponents.first } else { group = nil }
+                return .init(name: name, group: group, path: courseName + "/" + path)
+            }
+        }.sorted { (lhs, rhs) -> Bool in
+            lhs.name < rhs.name
+        }
+        
+        
+        let notesPath = "Public/logs/" + courseName + "/Notes.md"
+        let notesContent:String
+        do {
+            notesContent = try String(contentsOf:URL(fileURLWithPath: notesPath), encoding:.utf8)
+        }
+        catch {
+            notesContent = ""
+        }
+        
+        // Render the view index, with the context we've made
+        let context:Context = .init(name:courseName, simulations: simulations, notes: notesContent)
+        return req.view.render("instructor/course", context)
+    }
+    
+    // MARK: GET /instructeur/log_path
     // Renders a view for the instructor
-    app.get("instructor", "**") { req -> EventLoopFuture<View> in
+    app.get("instructeur", "**") { req -> EventLoopFuture<View> in
         struct Context: Encodable {
             struct Attachment: Encodable {
                 let url:String
@@ -161,6 +263,7 @@ func registerFrontEndRoutes(_ app: Application) throws {
             let assignments: [ControlPositionAssignment]?
             let attachments: [Attachment]?
             let displayEventsLocation: Bool
+            let courseNotes:String
             
             init(from log: Log, path:String) {
                 self.path = path
@@ -205,6 +308,21 @@ func registerFrontEndRoutes(_ app: Application) throws {
                     }
                 }
                 self.attachments = attachments
+                
+                // Read notes.md
+                var notesURL = URL(fileURLWithPath: "Public/logs")
+                notesURL.appendPathComponent(path)
+                notesURL.deleteLastPathComponent()
+                notesURL.deleteLastPathComponent()
+                notesURL.appendPathComponent("Notes.md", isDirectory: false)
+                let notes:String
+                do {
+                    notes = try String(contentsOf:notesURL, encoding:.utf8)
+                }
+                catch {
+                    notes = ""
+                }
+                self.courseNotes = notes
             }
         }
         
@@ -215,7 +333,7 @@ func registerFrontEndRoutes(_ app: Application) throws {
         case .failure(let error) :
             return renderLogErrorView(from: error, req: req)
         case .success(let log):
-            return req.view.render("log_instructor", Context(from: log, path: path))
+            return req.view.render("instructor/log_instructor", Context(from: log, path: path))
         }
     }
     
@@ -228,9 +346,89 @@ func registerFrontEndRoutes(_ app: Application) throws {
         return req.fileio.streamFile(at: path)
     }
     
-    // MARK: GET /pilot/role/log_path
+    // MARK: GET /pilote
+    // Renders a view containing all logs listed in alphabetical order
+    app.get("pilote") { req -> EventLoopFuture<View> in
+        
+        // Context type that will be passed to the view
+        struct Context: Encodable {
+            let courses: [Course]
+            
+            struct Course: Encodable {
+                let name: String
+                var simulations: [Simulation]
+            }
+            
+            struct Simulation: Encodable {
+                let name: String // Simulation name
+                let group: String? // Simulation's group within the course, this is optional
+                let path: String // Path to the log file
+                let defaultRole: String?
+            }
+        }
+        
+        // Enumerate files in the logs folder
+        let enumerator = FileManager.default.enumerator(atPath: "Public/logs")
+        guard let subpaths = enumerator?.allObjects as? [String]  else {
+            // If we cannot get the subpaths, render the error view
+            return req.view.render("error")
+        }
+        
+        struct Log {
+            let name:String
+            let course:String
+            let group:String?
+            let path:String
+            let role:String?
+        }
+            
+        // Create an array of logs from the content of the .simlog files
+        let logs = subpaths.compactMap { path -> String? in
+            // Filter files to only include .simlog files
+            guard URL(fileURLWithPath: path).pathExtension == "simlog" else { return nil }
+            
+            // Only include files in a subfolder
+            guard path.components(separatedBy: "/").count > 1 else { return nil }
+            
+            return path
+        }.compactMap { path -> Log? in
+            // Read the simulation name from the log file
+            switch log(atPath: path) {
+            case .failure : return nil // If the log file cannot be read, just ignore it
+            case .success(let log):
+                var pathComponents = path.components(separatedBy: "/")
+                let name = log.properties.name
+                let course = pathComponents.removeFirst()
+                let group:String?
+                if pathComponents.count > 1 { group = pathComponents.first } else { group = nil }
+                let role = log.pilot_logs.first?.role
+                return .init(name: name, course: course, group: group, path: path, role:role)
+            }
+        }.sorted { (lhs, rhs) -> Bool in
+            lhs.name < rhs.name
+        }
+        
+        var courses = [Context.Course]()
+        for log in logs {
+            let simulation = Context.Simulation(name: log.name, group: log.group, path: log.path, defaultRole: log.role)
+            if let courseIndex = courses.firstIndex(where: { $0.name == log.course }) {
+                courses[courseIndex].simulations.append(simulation)
+            } else {
+                courses.append(.init(name: log.course, simulations: [simulation]))
+            }
+        }
+        courses.sort { (lhs, rhs) -> Bool in
+            lhs.name < rhs.name
+        }
+        
+        // Render the view index, with the context we've made
+        let context:Context = .init(courses: courses)
+        return req.view.render("pilot/index", context)
+    }
+    
+    // MARK: GET /pilote/role/log_path
     // Renders a view for a pilot with the specified role
-    app.get("pilot", ":role", "**") { req -> EventLoopFuture<View> in
+    app.get("pilote", ":role", "**") { req -> EventLoopFuture<View> in
         struct Context: Encodable {
             let path:String
             let pilot_log: Log.PilotLog
@@ -270,7 +468,7 @@ func registerFrontEndRoutes(_ app: Application) throws {
             let context = Context(path: path, pilot_log: pilotLog, simulation_properties:.init(from: log.properties), roles:log.pilot_logs.map{ pilotLog in
                 pilotLog.role
             }, displayEventsLocation: atLeastOneEventContainsALocation)
-            return req.view.render("log_pilot", context)
+            return req.view.render("pilot/log_pilot", context)
         }
     }
     
